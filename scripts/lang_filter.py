efe140d2403d7702d76a2eaaaa3606e3f3b36454
Renate Hauser <renate.hauser@students.unibe.ch>
2021-05-03 13:32:26 +0200

    minor changes to lang_filter.py

--- scripts/lang_filter.py
+++ scripts/lang_filter.py
@@ -1,7 +1,6 @@
-#from textblob import TextBlob
-#from spacy_langdetect import LanguageDetector
-#import spacy
-#from googletrans import Translator
+# Small Python script for filtering out segments that are not in english from a dataset
+# author: Renate Hauser
+
 import pycld2 as cld2
 import sys
 
@@ -10,26 +9,15 @@ def main():
     infilename = sys.argv[1]
     outfilename = sys.argv[2]
 
-    #nlp = spacy.load('en')
-    #nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
-
-    #translator = Translator()
-
-
     with open(infilename, 'r', encoding='utf-8') as infile, open(outfilename, 'w', encoding='utf-8') as outfile:
         for line in infile:
-            #blob = TextBlob(line)
-            #lang = blob.detect_language()
-            #doc = nlp(line)
-            #lang = doc._.language['language']
-            #print(type(line))
-            #detection = translator.detect(line)
-            #lang = detection.lang
             _, _, _, detected_language = cld2.detect(line, returnVectors=True)
-            lang = detected_language[0][3]
-            if lang == 'en':
-                outfile.write(line)
-                #outfile.write('\n')
+            if detected_language:
+                lang = detected_language[0][3]
+                if lang == 'en':
+                    outfile.write(line)
+                else:
+                    continue
             else:
                 continue
 
